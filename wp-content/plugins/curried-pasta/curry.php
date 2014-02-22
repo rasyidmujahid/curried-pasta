c177ae0a44d429f7bc160b57f9925ce74e95534b
Abdurrasyid Mujahid <rasyidmujahid@gmail.com>
2014-02-22 16:00:20 +0700

    build google doc properties

--- wp-content/plugins/curried-pasta/curry.php
+++ wp-content/plugins/curried-pasta/curry.php
@@ -17,8 +17,10 @@ class Curry {
     protected $plugin_url;
 
     public function __construct(){
-        $this->plugin_path = dirname(__FILE__);
-        $this->plugin_url = WP_PLUGIN_URL . '/curried-pasta';
+        // $this->plugin_path = dirname(__FILE__);
+        // $this->plugin_url = WP_PLUGIN_URL . '/curried-pasta';
+        // wp_enqueue_script('curry_js', plugins_url('js/curry.js', __FILE__));
+        
         add_shortcode('gdoc', array($this, 'shortcode'));
     }
 
@@ -40,10 +42,41 @@ class Curry {
                                  // to set custom size, set the 'width' and 'height' params instead
         ), $atts));
 
-        return $this->build_iframe($id, $width, $height, $seamless, $size);
+        $htmls = [];
+
+        $method = 'open';
+        $google_doc_id = '1LCxqRRPChqg0zAnYnzhA36gR7ndAg1_M23nQH0XVCGM';
+        $url = "https://script.google.com/macros/s/AKfycbzggDVc5GFSNttaMkYU6pi8iy88exVSTiwyrKvwrr2nCj7m0rM/exec?method=". $method ."&gid=" . $google_doc_id;
+        
+        $args_json = file_get_contents($url);
+
+        $forms = $this->get_doc_args(json_decode($args_json, true));
+        foreach ($forms as $input) {
+            $htmls[] = $input;
+        }
+
+        $htmls[] = $this->get_embedded_doc($id, $width, $height, $seamless, $size);
+
+        return apply_filters('gdoc_output', implode("\n", $htmls));
     }
 
-    public function build_iframe($id, $width, $height, $seamless, $size)
+    public function get_doc_args($fields)
+    {
+        $htmls = [];
+        $htmls[] = '<form id="doc_param">';
+        foreach ($fields as $field) {
+            $htmls[] = $this->new_text_field($field['tag_string'], $this->underscored($field['tag_id']));
+        }
+        $htmls[] = '</form>';
+        return $htmls;
+    }
+
+    public function new_text_field($title, $id)
+    {
+        return '<span>'. $title .'</span><input type="text" id='. $id .'/><br/>';
+    }
+
+    public function get_embedded_doc($id, $width, $height, $seamless, $size)
     {
         if (!$id) return;
 
@@ -51,13 +84,22 @@ class Curry {
         // $output = '<iframe src="https://docs.google.com/file/d/' . $id .'/preview" width="' . $width . '" height="'. $height .'"></iframe>';
 
         # google doc preview
-        $output = '<iframe src="https://docs.google.com/document/d/' . $id . '/preview?embedded=false" width="' . $width . '" height="'. $height .'"></iframe>';
+        $output = '<iframe src="https://docs.google.com/document/d/' . $id . '/preview?embedded=false" width="' . $width . 
+            '" height="'. $height .'"></iframe>';
 
         # google doc viewer
         // $output = '<iframe src="https://docs.google.com/viewer?url=https://docs.google.com/document/d/'. $id .'/export?format%3Dpdf&id='. $id .'&embedded=true" style="width:680px; height:860px;" frameborder="0"></iframe>';
 
-        // return $output;
-        return apply_filters('gdoc_output', $output);
+        return $output;
+        // return apply_filters('gdoc_output', $output);
+    }
+
+    public function underscored($string)
+    {
+        $normalized = strtolower($string);
+        preg_match_all("/\{(.*)\}/", $normalized, $result, PREG_SET_ORDER);
+        $normalized = preg_replace("/\s+/", "_", $result[0][1]);
+        return $normalized;
     }
 
 }
